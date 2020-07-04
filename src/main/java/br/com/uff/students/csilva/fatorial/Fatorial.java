6768dc789ded01d5fee6d366f22a4d4eee5776d5
caiocesarsilva <caio.silva@marlo.com.au>
2020-07-04 19:15:09 -0300

    Novos testes.

--- src/main/java/br/com/uff/students/csilva/fatorial/Fatorial.java
+++ src/main/java/br/com/uff/students/csilva/fatorial/Fatorial.java
@@ -1,12 +1,25 @@
 package br.com.uff.students.csilva.fatorial;
 
+/**
+ * Classe Fatorial
+ */
 public class Fatorial {
 
-    public int fatorial(int numero){
+    /**
+     * Método para calcular fatorial
+     * @param numero número para cálculo do fatorial.
+     * @return resultado do fatorial.
+     */
+    public int fatorial(int numero) throws Exception {
         int fact = 1;
-        for (int i = 1; i<= numero; i++){
-            fact *= i;
+        if (numero > 0){
+            for (int i = 1; i<= numero; i++){
+                fact *= i;
+            }
+        }else{
+            throw new Exception();
         }
+
         return fact;
     }
 }
--- src/test/java/br/com/uff/students/csilva/fatorial/FatorialTest.java
+++ src/test/java/br/com/uff/students/csilva/fatorial/FatorialTest.java
@@ -7,15 +7,45 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Classe de teste para a classe Fatorial
+ * Parei de executar os testes apos testar os valores padrões e a exceção que o método pode lançar, para tentar cobrir
+ * a maior parte do código.
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class FatorialTest {
         private Fatorial fatorial;
 
+    /**
+     * Método para verificar se o cálculo do fatorial está correto.
+     */
     @Test
-    public void fatorialDeveSerVerdadeiro() {
-        int esperado = 720;
+    public void fatorialDeZeroDeveSerUm() throws Exception {
+        int esperado = 1;
         fatorial = new Fatorial();
 
-        assertEquals(esperado, fatorial.fatorial(6));
+        assertEquals(esperado, fatorial.fatorial(0));
+    }
+
+    /**
+     * Método para verificar se o cálculo do fatorial está correto.
+     */
+    @Test
+    public void fatorialDeUmDeveSerUm() throws Exception {
+        int esperado = 1;
+        fatorial = new Fatorial();
+
+        assertEquals(esperado, fatorial.fatorial(1));
+    }
+
+    /**
+     * Método para verificar se o cálculo do fatorial está correto.
+     */
+    @Test(expected = Exception.class)
+    public void fatorialDeNumeroNegativoDeveJogarExceção() throws Exception {
+        fatorial = new Fatorial();
+        fatorial.fatorial(-1);
+//        assertThrows(Exception.class, () -> fatorial.fatorial(-1));
+
     }
 }
